95e03d4153909f95c2567ca674f87e855f1821d9
Jason Whisnant <tasups@gmail.com>
2022-09-26 08:40:21 -0400

    Users components started

--- react-frontend-01-starting-setup/src/user/pages/Users.js
+++ react-frontend-01-starting-setup/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-  return <h2>Users Works!</h2>
+
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Emily Schwarz',
+      image: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 }
 
 export default Users
