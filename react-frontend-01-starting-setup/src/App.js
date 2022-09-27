a6c8637dc7a47581747d5d157f2f7cfadad7d123
Jason Whisnant <tasups@gmail.com>
2022-09-27 18:07:25 +0000

    input validators added

--- react-frontend-01-starting-setup/src/App.js
+++ react-frontend-01-starting-setup/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
 import Users from "./user/pages/Users"
-import MainNavigation from './shared/Navigation/MainNavigation'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 import NewPlace from './places/pages/NewPlace'
 import UserPlaces from './places/pages/UserPlaces'
 
