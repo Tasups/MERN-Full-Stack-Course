bdfbfb505839583580e0d5bc1f2c044537ed2cbe
Jason Whisnant <tasups@gmail.com>
2022-11-23 13:04:20 +0000

    signup added to auth

--- react-frontend-01-starting-setup/src/user/pages/Auth.js
+++ react-frontend-01-starting-setup/src/user/pages/Auth.js
@@ -9,9 +9,9 @@ import './Auth.css'
 
 const Auth = () => {
   
-  const [isLoginMode, setIsLoginMode] = useState(false)
+  const [isLoginMode, setIsLoginMode] = useState(true)
   
-  const [formState, inputHandler] = useForm({
+  const [formState, inputHandler, setFormData] = useForm({
     email: {
       value: '',
       isValid: false
@@ -29,6 +29,21 @@ const Auth = () => {
   }
   
   const switchModeHandler = () => {
+    if(!isLoginMode){
+      setFormData({
+        ...formState.inputs,
+        name: undefined,
+      }, formState.inputs.email.isValid && formState.inputs.password.isValid)
+    } else
+    {
+      setFormData({
+        ...formState.inputs,
+        name: {
+          value: '',
+          isValid: false
+        }
+      }, false)
+    }
     setIsLoginMode(prev => !prev)
   }
   
