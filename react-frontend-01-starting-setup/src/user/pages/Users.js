5ed1b6a73160a0d3fe90574fb604813e4b153033
Jason Whisnant <tasups@gmail.com>
2023-01-23 18:29:43 -0500

    Http hook to GET Users

--- react-frontend-01-starting-setup/src/user/pages/Users.js
+++ react-frontend-01-starting-setup/src/user/pages/Users.js
@@ -3,41 +3,27 @@ import React, { useEffect, useState } from 'react'
 import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import UsersList from '../components/UsersList'
+import { useHttpClient } from '../../shared/hooks/http-hook'
 
 const Users = () => {
-
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState()
+  const { isLoading, error, sendRequest, clearError } = useHttpClient()
   const [loadedUsers, setLoadedUsers] = useState()
 
   // useEffect doesn't like Promises, its just a bad idea put it within another code block within useEffect
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true)
+    const fetchUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users");
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message)
-        }
+        const responseData = await sendRequest("http://localhost:5000/api/users");
 
         setLoadedUsers(responseData.users);
-      } catch (error) {
-        setError(error.message)
-      }
-      setIsLoading(false);
+      } catch (error) {}
     }
-    sendRequest()
-  }, [])
-
-  const errorHandler = () => {
-    setError(null)
-  }
+    fetchUsers()
+  }, [sendRequest])
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && <div className="center">
         <LoadingSpinner />
       </div>}
