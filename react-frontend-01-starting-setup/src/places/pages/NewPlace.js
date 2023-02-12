d8c9fcf466c65a80ff82e467848bed71622d68b8
Jason Whisnant <tasups@gmail.com>
2023-02-12 16:07:16 -0500

    adding places works with image upload and file storage on server

--- react-frontend-01-starting-setup/src/places/pages/NewPlace.js
+++ react-frontend-01-starting-setup/src/places/pages/NewPlace.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
 import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
+import ImageUpload from '../../shared/components/FormElements/ImageUpload'
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
 import { useForm } from '../../shared/hooks/form-hook'
 import { useHttpClient } from '../../shared/hooks/http-hook'
@@ -12,47 +13,61 @@ import { AuthContext } from '../../shared/context/auth-context'
 import './FormPlace.css'
 
 const NewPlace = () => {
-  const auth = useContext(AuthContext)
-  const {isLoading, error, sendRequest, clearError} = useHttpClient()
-  
-  const [formState, inputHandler] = useForm({
-    title: {
-      value: '', 
-      isValid: false
-    },
-    description: {
-      value: '', 
-      isValid: false
-    },
-    address: {
-      value: '', 
-      isValid: false
-    },
-  },
-    false)
-  
-  const history = useHistory()
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  const placeSubmitHandler = async event => {
-    event.preventDefault()
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+      image: {
+        value: null,
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const history = useHistory();
+
+  const placeSubmitHandler = async (event) => {
+    event.preventDefault();
     try {
-      await sendRequest(
-        "http://localhost:5000/api/places",
-        "POST",
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
-          address: formState.inputs.address.value,
-          creator: auth.userId,
-        }),
-        { 'Content-Type': 'application/json' }
-      );
-      history.push('/')
-    } catch (err) {
-      
-    }
-    
-  }
+      const formData = new FormData();
+      formData.append("title", formState.inputs.title.value);
+      formData.append("description", formState.inputs.description.value);
+      formData.append("address", formState.inputs.address.value);
+      formData.append("creator", auth.userId);
+      formData.append("image", formState.inputs.image.value);
+      await sendRequest("http://localhost:5000/api/places", "POST", formData);
+      console.log(formState.inputs);
+      history.push("/");
+    } catch (err) {}
+  };
+  // try {
+  //   await sendRequest(
+  //     "http://localhost:5000/api/places",
+  //     "POST",
+  //     JSON.stringify({
+  //       title: formState.inputs.title.value,
+  //       description: formState.inputs.description.value,
+  //       address: formState.inputs.address.value,
+  //       creator: auth.userId,
+  //     }),
+  //     { 'Content-Type': 'application/json' }
+  //   );
+  //   history.push('/')
+  // }
 
   return (
     <React.Fragment>
@@ -67,6 +82,12 @@ const NewPlace = () => {
           validators={[VALIDATOR_REQUIRE()]}
           errorText="Please enter a valid title."
           onInput={inputHandler}
+        />
+        <ImageUpload
+          id="image"
+          center
+          onInput={inputHandler}
+          errorText="Please provide an image."
         />
         <Input
           id="description"
